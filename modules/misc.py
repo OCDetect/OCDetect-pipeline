80e8ed8870444c777c3b7b834f0652b37b51d6c3
Kristina K <kristina.kirsten@hpi.de>
2023-05-01 16:30:11 +0200

    method for calculating the initial hand washing time

--- modules/misc.py
+++ modules/misc.py
@@ -2,12 +2,38 @@ import datetime
 from typing import Tuple, Union
 import json
 from pathlib import Path
+import os
+import pandas as pd
 
-def initial_handwash_time():
+
+def initial_handwash_time(subject: str, config: dict) -> int:
     """
-    :return:
+    Calculates the initial hand washing time from the observed recording in the lab by taking the defined
+    start and end index from the first_hw_subfolder and searching for the file name string in the subject's
+    csv recordings file lists. If no initial hand washing was recorded, the mean time of 39s from all other
+    initial hw recordings is returned.
+    :param subject_id: The subject to be loaded
+    :param config: dict containing configuration information, e.g. folders, filenames or other settings
+    :return: the initial hand washing time in seconds if given, 39 as default otherwise
     """
-    return 39
+    avg_hand_wash_time = 39
+
+    first_hw_path = config["data_folder"] + config["first_hw_subfolder"]
+    first_hw_csv_names = [first_hw[7:] for first_hw in os.listdir(first_hw_path) if first_hw.endswith(".csv")]
+
+    csv_name_from_subject = [f for f in os.listdir(config["data_folder"] + config["prefix"] + subject) if f.endswith('.csv')]
+
+    for first_hw_csv_name in first_hw_csv_names:
+        if first_hw_csv_name in csv_name_from_subject:
+            csv = pd.read_csv(config["data_folder"] + config["prefix"] + subject + "/" + first_hw_csv_name, sep="\t")
+            first_hw_csv = pd.read_csv(first_hw_path + "/labels_" + first_hw_csv_name)
+            # since first hw csv is a csv with only one line, it comes as a series, so we need .values[0]
+            begin_ts = csv.iloc[first_hw_csv['start'].values[0]]['timestamp']
+            end_ts = csv.iloc[first_hw_csv['end'].values[0]]['timestamp']
+            return int((end_ts / 1000000000) - (begin_ts / 1000000000))
+
+    # take the average hand washing time if no initial recording was found
+    return avg_hand_wash_time
 
 
 def is_summertime_in2022(date: datetime.datetime, verbose: bool = False) -> bool:
