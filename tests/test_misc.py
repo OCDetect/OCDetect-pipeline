5ca126e6b895510432f7ff5260d44f2dcbe64f61
Kristina K <kristina.kirsten@hpi.de>
2023-06-13 14:12:16 +0200

    test magnitude function

---
+++ tests/test_misc.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from helpers.misc import calc_magnitude
+from helpers.definitions import Sensor
+
+
+def test_calc_magnitude():
+    # Sample input data
+    data = pd.DataFrame({
+        "acc x": [1, 2, 3],
+        "acc y": [4, 5, 6],
+        "acc z": [7, 8, 9]
+    })
+    expected_output = pd.DataFrame({
+        "acc x": [1, 2, 3],
+        "acc y": [4, 5, 6],
+        "acc z": [7, 8, 9],
+        "mag acc": [8.12403840463596, 9.643650760992955, 11.224972160321824]
+    })
+
+    output = calc_magnitude(data, Sensor.ACCELEROMETER)
+
+    # Compare the output with the expected result
+    # assert_frame_equal throws an AssertionError when two DataFrames aren't equal
+    pd.testing.assert_frame_equal(output, expected_output)
+
