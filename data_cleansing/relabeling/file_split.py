9f49a8c490b1239b08d20cb8c460283e58968f57
lorenzkautzsch <62888627+lorenzkautzsch@users.noreply.github.com>
2024-02-18 20:55:15 +0100

    incorporated code review feedback; file_split more efficient without iterrows

--- data_cleansing/relabeling/file_split.py
+++ data_cleansing/relabeling/file_split.py
@@ -12,57 +12,58 @@ if not os.path.exists(target_path):
 
 def process_file(file):
     try:
-        if file.endswith(".csv") and file.startswith("OCDetect_"): # only the recordings
-            df = pd.read_csv(os.path.join(origin_path, file))
-            df['datetime'] = pd.to_datetime(df['datetime'], format='%Y-%m-%d %H:%M:%S.%f')
+        if file.endswith(".csv") and file.startswith("OCDetect_"): # and file == "OCDetect_30_recording_11_a2ba690a-5a14-45ba-a1ae-fd2872a80bd4.csv": # and file == "OCDetect_03_recording_05_382535ec-9a0d-4359-b120-47f7605a22de.csv": # only the recordings
+            origin_data = pd.read_csv(os.path.join(origin_path, file)) # get data of the original file
+
+            origin_data['datetime'] = pd.to_datetime(origin_data['datetime'], format='%Y-%m-%d %H:%M:%S.%f') # convert 'datetime' to type datetime for comparisons
 
             origin_name = os.path.splitext(os.path.basename(file))[0] # returns name of file without .csv
             dir_path = os.path.join(target_path, origin_name) # directory name where new files should be stored (target_path/origin_file_name without .csv)
 
-            file_number = 1 # id for each subfile
+            # get indices of user labels in file
+            indices_user_label = list(enumerate(origin_data[origin_data['user yes/no'] == 1].index))
 
-            current = df.iloc[0]['datetime'] - pd.Timedelta(milliseconds=20) # set starting datetime minus a bit so the first row will be included
+            # indices of labels seperated, which need to be in one file
+            indices_close_labels = []
 
-            for index, row in df.iterrows():
-                # if the last user labeled row is not already in a file
-                if row['datetime'] > current:
-                    if row['user yes/no'] == 1:
-                        close_label = True
-                        current = row["datetime"]
-                        start = (current - pd.Timedelta(minutes=5))
+            last_idx = -1 # to include the first label as well
+            for idx, label_index in indices_user_label:
+                if idx > last_idx:
+                    close_labels = [label_index]
+                    forward = True
+                    last_idx = idx
+                    while forward and last_idx < len(indices_user_label)-1: # repeat as long as there are close user labels (5 min)
+                        forward = (origin_data.loc[indices_user_label[last_idx+1][1]]['datetime'] -
+                                   origin_data.loc[indices_user_label[last_idx][1]]['datetime'] <= pd.Timedelta(minutes = 5))
+                        if forward:
+                            close_labels.append(indices_user_label[last_idx+1][1])
+                            last_idx +=1
+                    indices_close_labels.append(close_labels) # append all user label indices for each file, that belong together
 
-                        # expanding the end of file (current) until there are no more user labels in the next 5 minutes
-                        while close_label:
-                            forward_df = df[(df['datetime'] > current) & (df['datetime'] <= (current + pd.Timedelta(minutes=5)))]
-                            close_label = ((forward_df['user yes/no'] == 1).any())
-                            if close_label:
-                                current = forward_df.loc[forward_df['user yes/no'] == 1, 'datetime'].max()
+            for idx, close_label in enumerate(indices_close_labels):
+                labeling_df = origin_data.loc[
+                    (origin_data['datetime'] >= origin_data.loc[close_label[0], 'datetime'] - pd.Timedelta(minutes=5)) &
+                    (origin_data['datetime'] <= origin_data.loc[close_label[-1], 'datetime']),
+                    ['datetime', 'acc x', 'acc y', 'acc z', 'gyro x', 'gyro y', 'gyro z', 'user yes/no']] # create new dataframe for all files
 
-                        labeling_df = df[(df['datetime'] >= start) & (df['datetime'] <= current)]
+                labeling_df['datetime'] = labeling_df['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S.%f') #convert to SS:MMMMMM for label studio
 
-                        labeling_df = labeling_df.loc[:, ['datetime', 'acc x', 'acc y', 'acc z', 'gyro x', 'gyro y', 'gyro z', 'user yes/no']] # reducing the file
-                        labeling_df['datetime'] = labeling_df['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S.%f')
+                if not os.path.exists(dir_path):
+                    os.mkdir(dir_path) # create folder for the corresponding files
 
-                        if not os.path.exists(dir_path):
-                            os.mkdir(dir_path)
-                        new_file_name = origin_name + "_" + str(file_number) + ".csv"
-
-                        labeling_df.to_csv(os.path.join(dir_path, new_file_name), index=False)
-
-                        file_number += 1
+                new_file_name = origin_name + "_" + str(idx+1) + ".csv"
+                labeling_df.to_csv(os.path.join(dir_path, new_file_name), index=False) #store each file with id
 
     except Exception as e:
         print(f"Error processing file {file}: {e}")
 
-
-# Use ProcessPoolExecutor to parallelize file processing
-with ProcessPoolExecutor(max_workers=63) as executor:
+# for parallel processing
+with ProcessPoolExecutor(max_workers=4) as executor:
     futures = [executor.submit(process_file, file) for file in os.listdir(origin_path)]
 
     for future in as_completed(futures):
         future.result()
 
-print("done")
 
 # INPUT
 # timestamp,datetime,acc x,acc y,acc z,gyro x,gyro y,gyro z,user yes/no,compulsive,urge,tense,ignore,relabeled
